4dd75f97edddf77a693689138a603ae3267b35e2
Christopher Wolf <Christopher.Wolf@eliagroup.eu>
2023-02-26 13:12:37 +0100

    Introduce class with dependency check constants
    By introducing this class a single point of responsibility was created
    instead of having magic strings spread in the code.

---
+++ src/Tasks/dependency-check-data-task/dependency-check-constants.ts
@@ -0,0 +1,5 @@
+export class DCConst {
+    public static readonly DependencyCheckVersionLatest: string = "latest";
+    public static readonly DependencyCheckFolderName: string = "dependency-check";
+    public static readonly DependencyCheckReleaseApi: string = "https://api.github.com/repos/jeremylong/DependencyCheck/releases";
+}
